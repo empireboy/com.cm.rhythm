803f42480111568f03dcf022717839f357ce478e
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2020-06-01 17:59:18 +0200

    Added an updater for RhythmEntity

---
+++ Scripts/RhythmEntityUpdater.cs
@@ -0,0 +1,35 @@
+﻿namespace CM.Rhythm
+{
+	public abstract class RhythmEntityUpdater<T>
+	{
+		protected abstract float Time { get; }
+
+		protected RhythmEntity<T> Entity { get; }
+		protected BeatCommand[] BeatCommands { get; }
+		protected int BeatCommandIndex { get; }
+
+		public RhythmEntityUpdater(RhythmEntity<T> entity)
+		{
+			Entity = entity;
+			BeatCommandIndex = 0;
+		}
+
+		public void Update()
+		{
+			if (Entity.Duration != 0)
+				UpdateDuration();
+
+			OnUpdate();
+		}
+
+		protected abstract void OnUpdate();
+
+		private void UpdateDuration()
+		{
+			Entity.Duration -= Time;
+
+			if (Entity.Duration <= 0)
+				Entity.Stop();
+		}
+	}
+}
