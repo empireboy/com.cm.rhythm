e4a657f2935aad6ff7fe4728de088bb7565c1d65
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2020-07-11 22:10:44 +0200

    Removed duration timer from RhythmEntityCommandUpdater. Now it only has one purpose: updating beat commands.

---
+++ Scripts/RhythmEntityCommandUpdater.cs
@@ -0,0 +1,38 @@
+﻿namespace CM.Rhythm
+{
+	public class RhythmEntityCommandUpdater<T>
+	{
+		protected RhythmEntity<T> Entity { get; }
+		protected BeatCommand[] BeatCommands { get; }
+		protected int BeatCommandIndex { get; private set; }
+
+		public RhythmEntityCommandUpdater(RhythmEntity<T> entity, BeatCommand[] beatCommands)
+		{
+			Entity = entity;
+			BeatCommands = beatCommands;
+			BeatCommandIndex = 0;
+		}
+
+		public void Update()
+		{
+			if (BeatCommands == null)
+				return;
+
+			if (BeatCommandIndex >= BeatCommands.Length)
+				return;
+
+			if (Entity.Time >= BeatCommands[BeatCommandIndex].StartTime)
+			{
+				BeatCommands[BeatCommandIndex].Execute();
+				BeatCommandIndex++;
+			}
+
+			OnUpdate();
+		}
+
+		protected virtual void OnUpdate()
+		{
+
+		}
+	}
+}
